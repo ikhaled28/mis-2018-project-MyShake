2b38e0d5bc7d826753a516b3d7c9a64ee1662c36
Izabul Khaled <khaled@nestedapps.com>
2018-06-06 15:51:00 +0200

    First commit

---
+++ app/src/main/java/com/nestedapps/izabulkhaled/myshake/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nestedapps.izabulkhaled.myshake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
